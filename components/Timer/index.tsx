94302c0e5ebcd946025cca7d7790be32779ba0fd
Tim Huitt <timhuitt@gmail.com>
2024-02-06 22:47:33 -0700

    add countdown

--- components/Timer/index.tsx
+++ components/Timer/index.tsx
@@ -1,19 +1,45 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import playSvg from '/play.svg'
 import pauseSvg from '/pause.svg'
 import styles from './timer.module.css'
 
 const Timer: React.FC = () => {
-  const [ play, setPlay ] = useState(true)
-  const [ min, setMin ] = useState(true)
+  const [ play, setPlay ] = useState<boolean>(false)
+  const [ minToggle, setMinToggle ] = useState(true)
+  const [ min, setMin ] = useState(0)
+  const [ sec, setSec ] = useState(3)
 
   const handleTimer = () => {
-    setPlay(!play)
+    setPlay(prevPlay => !prevPlay)
   }
   
   const handleType = () => {
-    setMin(!min)
+    setMinToggle(!minToggle)
   }
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout | null = null;
+
+    if (play && (min > 0 || sec > 0)) {
+        timer = setInterval(() => {
+            if (sec > 0) {
+                setSec(prevSec => prevSec - 1)
+            } else if (min > 0) {
+                setMin(prevMin => prevMin - 1)
+                setSec(59);
+            } else {
+              clearInterval(timer)
+              setPlay(false)
+            }
+        }, 1000)
+    }
+
+    return () => {
+        if (timer) {
+          clearInterval(timer)
+        }
+    };
+  }, [play, min, sec])
 
   return (
     <div className={styles.Timer}>
@@ -21,7 +47,7 @@ const Timer: React.FC = () => {
         <input type="text" placeholder="3"/>
         <div className={styles['time-type']} onClick={handleType}>
           <div className={styles['type-select']}>
-            { min ? (
+            { minToggle ? (
               <h2>min</h2>
             ) : (
               <h2>sec</h2>
@@ -31,14 +57,14 @@ const Timer: React.FC = () => {
       </div>
       <div className={styles['timer-controls']} onClick={ handleTimer }>
         { play ? (
-          <img src='/images/play.svg' />
-        ) : (
           <img src='/images/pause.svg' />
+        ) : (
+          <img src='/images/play.svg' />
         )}
       </div>
       <div className={styles['time-remaining']}>
         <div className={styles.timer}>
-          <h1>3:00</h1>
+          <h1>{min}:{sec < 10 ? '0' + sec : sec}</h1>
           <small>remaining</small>
         </div>
       </div>
