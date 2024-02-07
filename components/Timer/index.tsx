c9738951d96de91658ba8e885946be081d45409e
Tim Huitt <timhuitt@gmail.com>
2024-02-07 00:12:12 -0700

    add customizable timer

--- components/Timer/index.tsx
+++ components/Timer/index.tsx
@@ -1,20 +1,54 @@
-import { useState, useEffect } from 'react'
+import { useState, useRef, useEffect, ChangeEvent } from 'react'
 import playSvg from '/play.svg'
 import pauseSvg from '/pause.svg'
 import styles from './timer.module.css'
 
 const Timer: React.FC = () => {
   const [ play, setPlay ] = useState<boolean>(false)
-  const [ minToggle, setMinToggle ] = useState(true)
-  const [ min, setMin ] = useState(0)
-  const [ sec, setSec ] = useState(3)
+  const [ minToggle, setMinToggle ] = useState<boolean>(true)
+  const [ min, setMin ] = useState<number>(3)
+  const [ sec, setSec ] = useState<number>(0)
+  const minRef = useRef(3)
+  const secRef = useRef(0)
+
+  const setTime = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.currentTarget.value)
+    if (minToggle) {
+      if (value && !isNaN(value)) {
+        setMin(value)
+        minRef.current = value
+      } else {
+        setMin(0)
+        minRef.current = 0
+      }
+      setSec(0)
+    } else {
+      if (value && !isNaN(value)) {
+        setSec(value)
+        secRef.current = value
+      } else {
+        setSec(0)
+        secRef.current = 0
+      }
+      setMin(0)
+    }
+  }
 
   const handleTimer = () => {
     setPlay(prevPlay => !prevPlay)
   }
   
   const handleType = () => {
-    setMinToggle(!minToggle)
+    if (minToggle) {
+      setMin(0)
+      setSec(30)
+      secRef.current = 30
+    } else {
+      setSec(0)
+      setMin(3)
+      minRef.current = 3
+    }
+    setMinToggle(prev => !prev)
   }
 
   useEffect(() => {
@@ -32,6 +66,8 @@ const Timer: React.FC = () => {
               setPlay(false)
             }
         }, 1000)
+    } else {
+      setPlay(false)
     }
 
     return () => {
@@ -44,7 +80,11 @@ const Timer: React.FC = () => {
   return (
     <div className={styles.Timer}>
       <div className={styles['timer-settings']}>
-        <input type="text" placeholder="3"/>
+          <input 
+            type="text" 
+            value={minToggle ? minRef.current : secRef.current } 
+            onChange={setTime}
+          />
         <div className={styles['time-type']} onClick={handleType}>
           <div className={styles['type-select']}>
             { minToggle ? (
@@ -55,7 +95,7 @@ const Timer: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className={styles['timer-controls']} onClick={ handleTimer }>
+      <div className={styles['timer-controls']} onClick={handleTimer}>
         { play ? (
           <img src='/images/pause.svg' />
         ) : (
