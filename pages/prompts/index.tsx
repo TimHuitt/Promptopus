6c522a762c3fb8d14e34075f0182bf1f8d345b09
Tim Huitt <timhuitt@gmail.com>
2024-02-15 21:22:53 -0700

    add prompt rendering

--- pages/prompts/index.tsx
+++ pages/prompts/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 import Card from '../../components/Card'
@@ -9,11 +9,73 @@ import styles from './prompts.module.css'
 import Image from 'next/image'
 
 const Prompts: React.FC = () => {
+  const historyRef = useRef<string[]>([''])
+  const [ prompts, setPrompts ] = useState<string[]>(['Loading...'])
+  const [ count, setCount ] = useState<number[]>([4])
+  const [ theme, setTheme ] = useState<string[]>(['dragons'])
   const [ showPrompts, setShowPrompts ] = useState<boolean>(false)
+  const [ disabled, setDisabled ] = useState<boolean>(false)
   
-  const handleSubmit = () => {
+  const sendRequest = async (): Promise<any> => {
+    
+    const url = "http://localhost:4000/prompts/prompts";
+    // const url = "https://code-challenger-server-9e5cc705b6e9.herokuapp.com/prompts/prompts";
+    
+    console.log(historyRef)
+    try {
+      const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ content: `
+          count: ${count},
+          theme: ${theme},
+          history: ${historyRef.current}
+        `}),
+      });
+
+
+      if (res.ok) {
+        const jsonData = await res.json();
+        return jsonData;
+      } else {
+        throw new Error("Invalid request!");
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  const handleSubmit = async () => {
+
     setShowPrompts(true)
-  }
+    setDisabled(true)
+    
+    if (disabled) {
+      console.error('Please wait for the current request.');
+      return;
+    }
+
+    try {
+      const resData = await sendRequest();
+
+      if (resData) {
+        let response = typeof resData.response === 'string' 
+          ? JSON.parse(resData.response)
+          : resData.response
+        response = Object.values(response)
+        historyRef.current.push(...response)
+        setPrompts(response)
+      } else {
+        console.log('no data')
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setDisabled(false)
+    }
+  };
 
   return (
     <>
@@ -33,10 +95,10 @@ const Prompts: React.FC = () => {
       <div className={styles.Prompts} >
         { showPrompts ? (
           <div className={styles['prompt-card']}>
-            <Card prompt={ 'Volcano' } />
-            <Card prompt={ 'Butterfly' } />
-            <Card prompt={ 'Castle' } />
-            <Card prompt={ 'Guitar' } />
+            {prompts.map((prompt) => (
+              <Card prompt={ prompt } />
+            ))}
+            <button type="button" onClick={ handleSubmit }>Refresh!</button>
           </div>
         ) : (
           <div className={styles['prompt-settings']}>
