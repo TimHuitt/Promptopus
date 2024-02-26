e49c292ee88a2d3c3abd61abfa5c85d67caa8e13
Tim Huitt <timhuitt@gmail.com>
2024-02-26 10:44:47 -0700

    add tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
+import { useEffect } from 'react'
 import '../styles/global.css';
+import ReactGA from 'react-ga4'
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    ReactGA.initialize("G-EVH9V7EX44")
+  }, []);
+
   return <Component {...pageProps} />;
 }
